c485bc544dbd6cf09401d20a87c5c31549d897f1
benballervv <111202415785@mhs.dinus.ac.id>
2026-03-20 09:57:25 +0700

    Praktikum Mandiri

---
+++ oop-php/Mahasiswa.php
@@ -0,0 +1,31 @@
+ <?php
+ class Mahasiswa
+ {
+ //list of properties
+ public $nim;
+ public $nama;
+ public $programStudi;
+ //methods
+ function setData($nim, $nama, $programStudi)
+ {
+ $this->nim = $nim;
+ $this->nama = $nama;
+ $this->programStudi = $programStudi;
+ }
+ function getData()
+ {
+ return [
+ 'nim' => $this->nim,
+ 'nama' => $this->nama,
+ 'programStudi' => $this->programStudi
+ ];
+ }
+ //latihan 2
+ function tampilkanData()
+ {
+ echo "NIM: " . $this->nim . "<br>";
+ echo "Nama: " . $this->nama . "<br>";
+ echo "Program Studi: " . $this->programStudi . "<br><br>";
+ }
+
+ }
